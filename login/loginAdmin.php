f86cef123895f73fcfe64721af7d1d021e84af7b
DiegoValMu <diego.valderrama.mu@gmail.com>
2022-09-06 00:33:58 -0300

    correccion de falla en js

--- login/loginAdmin.php
+++ login/loginAdmin.php
@@ -4,17 +4,31 @@ require("../conexion/config.php");
 $RUT           = $_REQUEST['rutAdmin'];
 $DV            = $_REQUEST['DVA'];
 $password      = $_REQUEST['passAdmin'];
+
 $password = trim($password);  
-$SqlEventos   = ("SELECT * FROM administrador WHERE rut_adm = '".$RUT."'");
+
+
+$SqlEventos   = ("SELECT * FROM administrador WHERE rut_adm ='" .$RUT. "'");
 $result = mysqli_query($con, $SqlEventos);
+
+
 while ($data = mysqli_fetch_array($result)){ 
     if($data["contrasena_adm"] == $password && $data["digito_verificador_admin"] == $DV){
+        
         $_SESSION["nombre_usuario"] = $data["nomb_admin"];
         $_SESSION["apellidos_usuario"] = $data["apellidos_adm"];
         $_SESSION["id_usuario"] = $data["id_admin"];
+        mysqli_close($con); 
         echo true;
     }else{
+        session_destroy();
         echo false;
+       
     }
 }
+
+
+
+
+
 ?>
