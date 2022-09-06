3a915031cfc46ffaf5f1a997858bcad51c568d26
DiegoValMu <diego.valderrama.mu@gmail.com>
2022-09-05 23:43:47 -0300

    Conexion a la bd corrección

--- conexion/config.php
+++ conexion/config.php
@@ -1,11 +1,12 @@
 <?php
+/*
 $usuario  = "root";
 $password = "";
 $servidor = "localhost";
 $basededatos = "edifred";
 $con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
 $db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
-
+*/
 
 $usuario  = "E10software";
 $password = "E10software1137";
